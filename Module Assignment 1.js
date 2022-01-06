4718d75b7ab38e3250e97cdf4e4bfe9368cdd0cb
Magnus <89263593+magnusmosling@users.noreply.github.com>
2022-01-06 19:33:28 +0100

    Add files via upload

---
+++ Module Assignment 1.js
@@ -0,0 +1,57 @@
+//Module Assignment 1
+
+
+//.1
+var colour = "red";
+console.log (colour);
+//string
+
+var number = 20;
+console.log (number);
+//number
+
+var house = true;
+console.log (house);
+//boolean
+
+
+//.2
+var firsName = "magnus";
+var secondName = "mosling";
+
+console.log (firsName + " " + secondName);
+
+
+//.3
+var animal = "frog";
+
+console.log (typeof animal);
+console.log("The type of frog is a monkey");
+
+
+//.4
+var orderHasShipped = true;
+console.log ("The order shipped");
+
+var orderHasShipped = false;
+console.log ("The order did not ship");
+
+
+//.5
+
+for (var count = 7; count <= 13; count = count + 1)
+console.log (count)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
